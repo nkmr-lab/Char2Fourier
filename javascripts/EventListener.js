b6b032d82168d4557fb37274dd51b0584fa85ce0
satoken006 <seat.soil.74196901@gmail.com>
2019-07-16 01:41:59 +0900

    係数表示トグルボタンの挙動を修正

--- javascripts/EventListener.js
+++ javascripts/EventListener.js
@@ -1,9 +1,11 @@
 $(function(){
     $("#coefficientToggle").on("click", function(){
         if(isShowingCoef){
-            $(this).text("係数ON");
-        }else{
             $(this).text("係数OFF");
+            $(this).css("background-color", "blue");
+        }else{
+            $(this).text("係数ON");
+            $(this).css("background-color", "#e8822a");
         }
         isShowingCoef = !isShowingCoef;
     });
